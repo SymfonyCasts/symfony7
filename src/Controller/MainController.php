87b20d8a79b490a768b5538a4c45673965d6fafc
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:13 -0500

    [tuts-apply-diff] Applying existing diff file for step twig-extend-abstractcontroller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
