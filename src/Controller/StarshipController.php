87614e1047d3bc3213c33b5ae6d70095fd397a39
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:18 -0500

    [tuts-apply-diff] Applying existing diff file for step routing-give-show-route-a-name

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController
 {
-    #[Route('/starships/{id<\d+>}')]
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
     public function show(int $id, StarshipRepository $repository): Response
     {
         $ship = $repository->find($id);
