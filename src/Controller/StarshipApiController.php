5db26551c35dcd031884ecfb7ccab0ecafe168e4
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:16 -0500

    [tuts-apply-diff] Applying existing diff file for step services-autowire-loggerinterface

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        dd($logger);
         $starships = [
             new Starship(
                 1,
