ffb1a66e41425b796bf73dcf544324536eb5b1b2
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:16 -0500

    [tuts-apply-diff] Applying existing diff file for step services-add-findall-to-repo

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -15,30 +15,7 @@ class StarshipApiController extends AbstractController
     public function getCollection(LoggerInterface $logger, StarshipRepository $repository): Response
     {
         $logger->info('Starship collection retrieved');
-        dd($repository);
-        $starships = [
-            new Starship(
-                1,
-                'USS LeafyCruiser (NCC-0001)',
-                'Garden',
-                'Jean-Luc Pickles',
-                'taken over by Q'
-            ),
-            new Starship(
-                2,
-                'USS Espresso (NCC-1234-C)',
-                'Latte',
-                'James T. Quick!',
-                'repaired',
-            ),
-            new Starship(
-                3,
-                'USS Wanderlust (NCC-2024-W)',
-                'Delta Tourist',
-                'Kathryn Journeyway',
-                'under construction',
-            ),
-        ];
+        $starships = $repository->findAll();
 
         return $this->json($starships);
     }
--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -2,6 +2,38 @@
 
 namespace App\Repository;
 
-class StarshipRepository
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipRepository extends AbstractController
 {
+    public function findAll(): array
+    {
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+        ];
+    }
 }
