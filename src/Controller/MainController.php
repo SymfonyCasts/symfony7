cb44520028a3264c772e89d5f9673901361353c4
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:14 -0500

    [tuts-apply-diff] Applying existing diff file for step twig-pass-in-associative-array

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,9 +12,16 @@ class MainController extends AbstractController
     public function homepage(): Response
     {
         $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
 
         return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
         ]);
     }
 }
