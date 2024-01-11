ce1e58237938f80edeb8f05d2d36f10aba9bc9ef
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:12 -0500

    [tuts-apply-diff] Applying existing diff file for step first-page-return-a-response

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController
@@ -9,6 +10,6 @@ class MainController
     #[Route('/')]
     public function homepage()
     {
-
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
     }
 }
