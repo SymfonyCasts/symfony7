c6b9c26ae25d5899d4e101db27db505bd2d4f092
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:18 -0500

    [tuts-apply-diff] Applying existing diff file for step routing-create-starship-show-page

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}')]
+    public function show(int $id): Response
+    {
+        dd($id);
+    }
+}
