4a05bf2fe6d60370fc22c6e2d442a9e7fcf7df2c
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:16 -0500

    [tuts-apply-diff] Applying existing diff file for step services-create-starshiprepository

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use App\Repository\StarshipRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +12,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(LoggerInterface $logger): Response
+    public function getCollection(LoggerInterface $logger, StarshipRepository $repository): Response
     {
         $logger->info('Starship collection retrieved');
+        dd($repository);
         $starships = [
             new Starship(
                 1,
