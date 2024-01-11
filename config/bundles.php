24c22c188a1f18c19774b111370cd4a0bff0acce
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:20 -0500

    [tuts-apply-diff] Applying existing diff file for step maker-install-it

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
