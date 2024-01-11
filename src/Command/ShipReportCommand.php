cfc478175f5202b460d3f3a659a70f31203a6982
Ryan Weaver <ryan@symfonycasts.com>
2024-01-11 16:09:20 -0500

    [tuts-apply-diff] Applying existing diff file for step maker-use-the-progress-bar

--- src/Command/ShipReportCommand.php
+++ src/Command/ShipReportCommand.php
@@ -42,6 +42,12 @@ class ShipReportCommand extends Command
             // ...
         }
 
+        $io->progressStart(100);
+        for ($i = 0; $i < 100; ++$i) {
+            $io->progressAdvance();
+            usleep(10000);
+        }
+
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
