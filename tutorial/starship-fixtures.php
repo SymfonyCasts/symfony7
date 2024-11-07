8ab460adf96a5ae8ff8cff0fdfd459044f811295
Kevin Bond <kevinbond@gmail.com>
2024-11-07 09:32:32 -0500

    update tutorial fixtures

--- tutorial/starship-fixtures.php
+++ tutorial/starship-fixtures.php
@@ -23,3 +23,27 @@ $ship3->setClass('Delta Tourist');
 $ship3->setCaptain('Kathryn Journeyway');
 $ship3->setStatus(StarshipStatusEnum::WAITING);
 $ship3->setArrivedAt(new \DateTimeImmutable('-1 month'));
+
+StarshipFactory::createOne([
+    'name' => 'USS LeafyCruiser (NCC-0001)',
+    'class' => 'Garden',
+    'captain' => 'Jean-Luc Pickles',
+    'status' => StarshipStatusEnum::IN_PROGRESS,
+    'arrivedAt' => new \DateTimeImmutable('-1 day'),
+]);
+
+StarshipFactory::createOne([
+    'name' => 'USS Espresso (NCC-1234-C)',
+    'class' => 'Latte',
+    'captain' => 'James T. Quick!',
+    'status' => StarshipStatusEnum::COMPLETED,
+    'arrivedAt' => new \DateTimeImmutable('-1 week'),
+]);
+
+StarshipFactory::createOne([
+    'name' => 'USS Wanderlust (NCC-2024-W)',
+    'class' => 'Delta Tourist',
+    'captain' => 'Kathryn Journeyway',
+    'status' => StarshipStatusEnum::WAITING,
+    'arrivedAt' => new \DateTimeImmutable('-1 month'),
+]);
