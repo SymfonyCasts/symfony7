a436a969b67db1b522fb710cda7d0a45a12ed05f
Ryan Weaver <ryan@symfonycasts.com>
2025-03-09 11:21:56 -0400

    tuts hero working...

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -128,30 +128,6 @@ class Starship
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
     public function checkIn(?\DateTimeImmutable $arrivedAt = null): static
     {
         $this->arrivedAt = $arrivedAt ?? new \DateTimeImmutable('now');
