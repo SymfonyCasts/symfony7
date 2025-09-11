bae2ff727923697356180b919d6041fbb04296ed
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:24 +0200

    [tuts-apply-diff] Applying existing diff file for step manytomanyextra-remove-droids-to-fix-fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -48,7 +48,7 @@ class AppFixtures extends Fixture
         DroidFactory::createMany(100);
 
         StarshipFactory::createMany(100, fn() => [
-            'droids' => DroidFactory::randomRange(1, 5),
+            //'droids' => DroidFactory::randomRange(1, 5),
         ]);
         StarshipPartFactory::createMany(100);
     }
