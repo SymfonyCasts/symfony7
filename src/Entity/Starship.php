6156f605e34f4b9be1204037b03aa5dbcc49eec8
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:19 +0200

    [tuts-apply-diff] Applying existing diff file for step onetomany-fetch-extra-lazy

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -42,7 +42,7 @@ class Starship
     /**
      * @var Collection<int, StarshipPart>
      */
-    #[ORM\OneToMany(targetEntity: StarshipPart::class, mappedBy: 'starship', orphanRemoval: true)]
+    #[ORM\OneToMany(targetEntity: StarshipPart::class, mappedBy: 'starship', fetch: 'EXTRA_LAZY', orphanRemoval: true)]
     #[ORM\OrderBy(['name' => 'ASC'])]
     private Collection $parts;
 
