7d5ee1f89c34a74779cd9dd3209fee3df2c9089b
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:24 +0200

    [tuts-apply-diff] Applying existing diff file for step manytomanyextra-make-entity-starshipdroid

--- src/Entity/Droid.php
+++ src/Entity/Droid.php
@@ -21,8 +21,15 @@ class Droid
     #[ORM\Column(length: 255)]
     private ?string $primaryFunction = null;
 
+    /**
+     * @var Collection<int, StarshipDroid>
+     */
+    #[ORM\OneToMany(targetEntity: StarshipDroid::class, mappedBy: 'droid')]
+    private Collection $starshipDroids;
+
     public function __construct()
     {
+        $this->starshipDroids = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -76,6 +83,36 @@ class Droid
     {
         if ($this->starships->removeElement($starship)) {
             $starship->removeDroid($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StarshipDroid>
+     */
+    public function getStarshipDroids(): Collection
+    {
+        return $this->starshipDroids;
+    }
+
+    public function addStarshipDroid(StarshipDroid $starshipDroid): static
+    {
+        if (!$this->starshipDroids->contains($starshipDroid)) {
+            $this->starshipDroids->add($starshipDroid);
+            $starshipDroid->setDroid($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStarshipDroid(StarshipDroid $starshipDroid): static
+    {
+        if ($this->starshipDroids->removeElement($starshipDroid)) {
+            // set the owning side to null (unless already changed)
+            if ($starshipDroid->getDroid() === $this) {
+                $starshipDroid->setDroid(null);
+            }
         }
 
         return $this;
