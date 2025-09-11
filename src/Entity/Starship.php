5d5f2ebc408c5e48d4891af51e9b5ec4b5457f08
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:19 +0200

    [tuts-apply-diff] Applying existing diff file for step orderby-add-name-asc

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -43,6 +43,7 @@ class Starship
      * @var Collection<int, StarshipPart>
      */
     #[ORM\OneToMany(targetEntity: StarshipPart::class, mappedBy: 'starship', orphanRemoval: true)]
+    #[ORM\OrderBy(['name' => 'ASC'])]
     private Collection $parts;
 
     public function __construct()
