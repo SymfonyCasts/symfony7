2646af561c124d87f7843e287df3824abb2cf53f
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:20 +0200

    [tuts-apply-diff] Applying existing diff file for step part-query-for-parts-and-render

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipPartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PartController extends AbstractController
 {
     #[Route('/parts', name: 'app_part_index')]
-    public function index(): Response
+    public function index(StarshipPartRepository $repository): Response
     {
+        $parts = $repository->findAll();
+
         return $this->render('part/index.html.twig', [
-            'controller_name' => 'PartController',
+            'parts' => $parts,
         ]);
     }
 }
