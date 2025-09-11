f386ecd7f05d3d90d64ec562a8b74ab406c6af06
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-09-11 13:06:23 +0200

    [tuts-apply-diff] Applying existing diff file for step manytomany-render-droid-names-on-homepage

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -220,4 +220,9 @@ class Starship
 
         return $this;
     }
+
+    public function getDroidNames(): string
+    {
+        return implode(', ', $this->droids->map(fn(Droid $droid) => $droid->getName())->toArray());
+    }
 }
