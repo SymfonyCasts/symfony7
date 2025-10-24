f1b96ac4fd2da2300cc8d206852968bc50671c2b
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:10 +0200

    [tuts-apply-diff] Applying existing diff file for step orphanremoval-add-orphanremoval-and-dump

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -42,7 +42,7 @@ class Starship
     /**
      * @var Collection<int, StarshipPart>
      */
-    #[ORM\OneToMany(targetEntity: StarshipPart::class, mappedBy: 'starship')]
+    #[ORM\OneToMany(targetEntity: StarshipPart::class, mappedBy: 'starship', orphanRemoval: true)]
     private Collection $parts;
 
     public function __construct()
