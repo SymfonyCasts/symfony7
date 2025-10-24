1da1e5c5f4ceaffdf2ae1e88dc174efc3d38be09
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:09 +0200

    [tuts-apply-diff] Applying existing diff file for step owninginverse-use-real

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,7 +13,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $starship = StarshipFactory::createOne();
+        $starship = StarshipFactory::createOne()->_real();
         $part1 = new StarshipPart();
         $part1->setName('Warp Core');
         $part1->setPrice(1000);
