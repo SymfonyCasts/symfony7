c93a682ebe7cf5d834d621ea35a6dd01be5740f7
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:13 +0200

    [tuts-apply-diff] Applying existing diff file for step manytomany-make-factory-droid

---
+++ src/Factory/DroidFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Droid;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Droid>
+ */
+final class DroidFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Droid::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->text(255),
+            'primaryFunction' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Droid $droid): void {})
+        ;
+    }
+}
