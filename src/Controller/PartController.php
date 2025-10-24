bee48a675ca485113183e7d59d8bf792077ab487
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:13 +0200

    [tuts-apply-diff] Applying existing diff file for step search-query-builder-with-fuzzy-search

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -14,9 +14,8 @@ final class PartController extends AbstractController
     public function index(StarshipPartRepository $repository, Request $request,): Response
     {
         $query = $request->query->get('query');
-        dd($query);
 
-        $parts = $repository->findAllOrderedByPrice();
+        $parts = $repository->findAllOrderedByPrice($query);
 
         return $this->render('part/index.html.twig', [
             'parts' => $parts,
