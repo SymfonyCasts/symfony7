04545c340e3a919bcf9d15ff9df761e5253f6ddc
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:11 +0200

    [tuts-apply-diff] Applying existing diff file for step criteria-move-into-repo-method

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\StarshipPartRepository;
 use App\Repository\StarshipRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -164,9 +165,7 @@ class Starship
      */
     public function getExpensiveParts(): Collection
     {
-        $criteria = Criteria::create()->andWhere(Criteria::expr()->gt('price', 50000));
-
-        return $this->parts->matching($criteria);
+        return $this->parts->matching(StarshipPartRepository::createExpensiveCriteria());
     }
 
     public function addPart(StarshipPart $part): static
--- src/Repository/StarshipPartRepository.php
+++ src/Repository/StarshipPartRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\StarshipPart;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,6 +15,11 @@ class StarshipPartRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, StarshipPart::class);
+    }
+
+    public static function createExpensiveCriteria(): Criteria
+    {
+        return Criteria::create()->andWhere(Criteria::expr()->gt('price', 50000));
     }
 
 //    /**
