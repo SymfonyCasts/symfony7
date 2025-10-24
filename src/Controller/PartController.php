3dddba511e599cd1e020595819ec4991c6c7c98d
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:13 +0200

    [tuts-apply-diff] Applying existing diff file for step search-read-query-param-from-request

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Repository\StarshipPartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PartController extends AbstractController
 {
     #[Route('/parts', name: 'app_part_index')]
-    public function index(StarshipPartRepository $repository): Response
+    public function index(StarshipPartRepository $repository, Request $request,): Response
     {
+        $query = $request->query->get('query');
+        dd($query);
+
         $parts = $repository->findAllOrderedByPrice();
 
         return $this->render('part/index.html.twig', [
