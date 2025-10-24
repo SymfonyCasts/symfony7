7bc58a017c5116c7bbe74a8db196779575b01ae0
Victor Bocharsky <bocharsky.bw@gmail.com>
2025-10-24 18:31:08 +0200

    [tuts-apply-diff] Applying existing diff file for step starshippart-create-in-fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Starship;
+use App\Entity\StarshipPart;
 use App\Entity\StarshipStatusEnum;
 use App\Factory\StarshipFactory;
 use App\Factory\StarshipPartFactory;
@@ -20,6 +22,20 @@ class AppFixtures extends Fixture
             'arrivedAt' => new \DateTimeImmutable('-1 day'),
         ]);
 
+        $starship = new Starship();
+        $starship->setName('USS Taco Tuesday');
+        $starship->setClass('Tex-Mex');
+        $starship->checkIn();
+        $starship->setCaptain('James T. Nacho');
+        $manager->persist($starship);
+
+        $part = new StarshipPart();
+        $part->setName('spoiler');
+        $part->setNotes('There\'s no air drag in space, but it looks cool.');
+        $part->setPrice(500);
+        $manager->persist($part);
+        $manager->flush();
+
         StarshipFactory::createOne([
             'name' => 'USS Espresso (NCC-1234-C)',
             'class' => 'Latte',
@@ -37,6 +53,5 @@ class AppFixtures extends Fixture
         ]);
 
         StarshipFactory::createMany(20);
-        StarshipPartFactory::createMany(50);
     }
 }
