41c7b4eb723746a5621ba2ea465f41adac4213f5
Kevin Bond <kevinbond@gmail.com>
2026-03-02 11:32:03 -0500

    add foundry

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
