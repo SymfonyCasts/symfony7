3838cb39f6636d216a04a6507775803694d6e389
Victor Bocharsky <bocharsky.bw@gmail.com>
2026-07-09 19:08:22 +0200

    [tuts-apply-diff] Applying existing diff file for step 6-post-logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,7 +24,7 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_logout', methods: 'POST')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
