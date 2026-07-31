1ac2b2ff0b21c4e2f1faf0fc03c5ed4220b81c07
Victor Bocharsky <bocharsky.bw@gmail.com>
2026-07-31 16:35:43 +0200

    Dix migration and disable transactional feature

--- migrations/Version20260731142632.php
+++ migrations/Version20260731142632.php
@@ -32,4 +32,9 @@ final class Version20260731142632 extends AbstractMigration
         $this->addSql('DROP INDEX IDX_8D93D6499B24DF5 ON user');
         $this->addSql('ALTER TABLE user DROP starship_id');
     }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
 }
